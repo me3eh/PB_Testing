305b1cec59648cbbe5a9bee9c20f3a0ec4b7b6ff
me3eh <matt30002000@gmail.com>
2022-10-09 18:12:13 +0200

    Add configuration file with database handling

--- src/preparation/configuration_gui.py
+++ src/preparation/configuration_gui.py
@@ -1,7 +1,6 @@
 import PySimpleGUI as sg
 from configparser import ConfigParser
-from sqlite.base import database_retrieve_urls
-from sqlite.base import database_retrieve_attribute
+from sqlite import database
 from site_scraping.site_info import get_browser_attributes
 
 attributes = ['ids', 'css_classes', 'input_names']
@@ -11,7 +10,6 @@ def save_configuration(config):
         config.write(f)
 
 def fill_attribute_listbox(window):
-    print(window['-USER-URLS-'].get())
     checked_url = window['-USER-URLS-'].get()
     if not checked_url:
         sg.popup("Not checked anything")
@@ -19,8 +17,9 @@ def fill_attribute_listbox(window):
         attribute = window['-ATTRIBUTE-SELECT-'].get()
         print(attribute)
         print(checked_url[0])
-        values = database_retrieve_attribute(attribute=attribute, url=checked_url[0][0])
-        if values:
+        values = database.retrieve_attribute(attribute=attribute, url=checked_url[0])
+        print(values)
+        if values and values[0][0] != None:
             window['-ATTRIBUTES-LISTBOX-'].update(eval(values[0][0]))
         else:
             window['-ATTRIBUTES-LISTBOX-'].update([])
@@ -53,7 +52,7 @@ def launch_configuration():
                         sg.Text('', expand_y=True),
                     ],
                     [
-                        sg.Listbox(values=database_retrieve_urls('original_urls'), k='-SCANNED-URLS-', size=(20, 20))
+                        sg.Listbox(values=database.retrieve_urls('original_urls'), k='-SCANNED-URLS-', size=(20, 20))
                     ]
                 ], k='layout_principal', expand_x=True),
             sg.Column(
@@ -72,7 +71,7 @@ def launch_configuration():
                         sg.Text('', expand_y=True),
                     ],
                     [
-                        sg.Listbox(values=database_retrieve_urls('urls_and_attributes'), enable_events=True,
+                        sg.Listbox(values=database.retrieve_urls('urls_and_attributes'), enable_events=True,
                                    k='-USER-URLS-', size=(20, 20))
                     ]
                 ], k='layout_principal', expand_x=True),
@@ -101,7 +100,8 @@ def launch_configuration():
         [
             sg.Input("Yas", k='-INPUT-ANYTHING-'),
             sg.Button("Save that", k='-SAVE-ANYTHING-'),
-            sg.Button("Add that", k='-SAVE-ANYTHING-')
+            sg.Button("Add that", k='-ADD-ANYTHING-', button_color='green'),
+            sg.Button("Delete selected", k='-DELETE-SELECTED-', button_color='red')
         ]
     ]
     window = sg.Window("Configuration for testing", layout, resizable=True, finalize=True)
@@ -127,7 +127,10 @@ def launch_configuration():
                 sg.popup("Not checked anything")
             else:
                 domain = config.get('main', 'domain')
-                print(get_browser_attributes(domain + '/' + ''.join(checked_url)))
+                browser_attributes = get_browser_attributes(domain + '/' + ''.join(checked_url))
+                print(browser_attributes)
+                database.save_attributes(checked_url, css_classes=browser_attributes['class'],
+                                         ids=browser_attributes['id'], input_names=browser_attributes['name'])
                 print(domain + '/' + ''.join(checked_url))
         elif event == '-ADD-':
             checked_url = window['-SCANNED-URLS-'].get()
@@ -136,11 +139,12 @@ def launch_configuration():
             else:
                 urls = window['-USER-URLS-'].get_list_values()
                 print(urls)
-                new_urls = urls + checked_url
-                print(new_urls)
-                window['-USER-URLS-'].update(new_urls)
-                # values = database_retrieve_attribute(attribute=attribute, url=checked_url[0][0])
-                # window['-ATTRIBUTES-LISTBOX-'].update(eval(values[0][0]))
+                print(checked_url)
+                if checked_url[0] in urls:
+                    sg.popup("Not checked anything in scanned_urls section")
+                else:
+                    new_urls = urls + checked_url
+                    window['-USER-URLS-'].update(new_urls)
         elif event == '-SAVE-ANYTHING-':
             input = window['-INPUT-ANYTHING-'].get()
             checked_url = window['-USER-URLS-'].get()
@@ -149,6 +153,36 @@ def launch_configuration():
             else:
                 index = window['-USER-URLS-'].get_indexes()[0]
                 values = window['-USER-URLS-'].get_list_values()
-                values[index] = input
+                if input in values:
+                    sg.popup("Nope, that is already in used urls")
+                else:
+                    database.rename_url(values[index], input)
+                    values[index] = input
+                    window['-USER-URLS-'].update(values)
+                    window['-USER-URLS-'].widget.selection_set(index)
+        elif event == '-ADD-ANYTHING-':
+            input = window['-INPUT-ANYTHING-'].get()
+            selected_index = window['-USER-URLS-'].get_indexes()
+            selected = False
+            if selected_index:
+                selected_index = selected_index[0]
+                selected = True
+            values = window['-USER-URLS-'].get_list_values()
+            if input in values:
+                sg.popup("Nope, that is already in used urls")
+            else:
+                values.append(input)
                 window['-USER-URLS-'].update(values)
-                window['-USER-URLS-'].widget.selection_set(index)
+                if selected:
+                    window['-USER-URLS-'].widget.selection_set(selected_index)
+                database.add_url(input)
+        elif event == '-DELETE-SELECTED-':
+            selected_index = window['-USER-URLS-'].get_indexes()
+            if not selected_index:
+                sg.popup("Not checked anything in scanned_urls section")
+            else:
+                values = window['-USER-URLS-'].get_list_values()
+                database.delete_url(values[selected_index[0]])
+                del values[selected_index[0]]
+                window['-USER-URLS-'].update(values)
+            window['-ATTRIBUTES-LISTBOX-'].update([])
--- src/sqlite/database.py
+++ src/sqlite/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def database_setup():
+def setup():
     conn = sqlite3.connect('resources_for_testing/handy.sqlite')
 
     c = conn.cursor()
@@ -33,7 +33,7 @@ def database_setup():
     conn.close()
 
 
-def database_save_urls(objects):
+def save_urls(objects):
     conn = sqlite3.connect('resources_for_testing/handy.sqlite')
 
     c = conn.cursor()
@@ -44,16 +44,18 @@ def database_save_urls(objects):
     conn.close()
 
 
-def database_retrieve_urls(database_name):
+def retrieve_urls(database_name):
     conn = sqlite3.connect('resources_for_testing/handy.sqlite')
 
+    #line below will return array of strings, instead of array of tuples
+    conn.row_factory = lambda cursor, row: row[0]
     c = conn.cursor()
     urls = c.execute(f"SELECT url from {database_name}").fetchall()
     conn.close()
     return urls
 
 
-def database_retrieve_attribute(attribute, url):
+def retrieve_attribute(attribute, url):
     conn = sqlite3.connect('resources_for_testing/handy.sqlite')
 
     c = conn.cursor()
@@ -61,5 +63,44 @@ def database_retrieve_attribute(attribute, url):
     conn.close()
     return urls
 
+
+def add_url(url):
+    conn = sqlite3.connect('resources_for_testing/handy.sqlite')
+
+    c = conn.cursor()
+    c.execute(f"INSERT INTO urls_and_attributes(url) values('{url}')")
+    conn.commit()
+    conn.close()
+
+def delete_url(url):
+    conn = sqlite3.connect('resources_for_testing/handy.sqlite')
+
+    c = conn.cursor()
+    c.execute(f"DELETE FROM urls_and_attributes where url='{url}'")
+    conn.commit()
+    conn.close()
+
+def save_attributes(url, ids, css_classes, input_names):
+    conn = sqlite3.connect('resources_for_testing/handy.sqlite')
+
+    #line below will return array of strings, instead of array of tuples
+    # conn.row_factory = lambda cursor, row: row[0]
+    print(ids)
+    temp_ids = repr(ids)
+    temp_css_classes = repr(css_classes)
+    temp_input_names = repr(input_names)
+    c = conn.cursor()
+    k = f'UPDATE urls_and_attributes set ids=\"{temp_ids}\", css_classes=\"{temp_css_classes}\", input_names=\"{temp_input_names}\" where url=\"{url}\"'
+    print(k)
+    c.execute(k)
+    conn.commit()
+    conn.close()
+def rename_url(url, new_url):
+    conn = sqlite3.connect('resources_for_testing/handy.sqlite')
+    c = conn.cursor()
+    c.execute(f'UPDATE urls_and_attributes set url=\'{new_url}\' where url=\'{url}\'')
+    conn.commit()
+    conn.close()
 # c = database_retrieve_urls('urls_and_attributes')
 # print(c)
+# database_save_attributes("/spanie", ids=["less go"], css_classes=['cos innego'], input_names=['lecim', 'na', 'szczecin'])
